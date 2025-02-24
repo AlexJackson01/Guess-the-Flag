541ce8cabe8448c2d2a84d78a88f2ad1d91458c3
Alex Jackson <ajjackson22@gmail.com>
2025-02-24 18:28:55 +0000

    Practicing

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello")
+            Text("World!")
+            
+            Button {
+                showingAlert = true
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+            }
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+            
+            Image(systemName: "pencil")
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+            
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.indigo.gradient)
+
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            
+            Text("Content")
+                .foregroundStyle(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+            
+        }
+        .ignoresSafeArea()
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Read")
+        }
+        
+        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+        
+        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
+        
     }
 }
 
