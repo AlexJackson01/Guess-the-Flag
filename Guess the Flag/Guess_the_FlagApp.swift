839922d8798e6e1b2175f80f5ec794a4b5fe443c
Alex Jackson <ajjackson22@gmail.com>
2025-02-24 17:21:41 +0000

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Alex Jackson on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
